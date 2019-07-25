dbf285c68e5ee5d6bd051c9e4190fc0ba3b495cb
Charles Richards <richardsc@bxscience.edu>
2019-07-25 15:13:08 -0400

    se_text return something glove path added

--- C_1.py
+++ C_1.py
@@ -1,17 +1,19 @@
 import gensim
 import numpy as np
 from gensim.models.keyedvectors import KeyedVectors
-glove = KeyedVectors.load_word2vec_format(path, binary=False)
+import gensim
+
+glove = KeyedVectors.load_word2vec_format(r"./glove.6B.50d.txt.w2v", binary=False)
 from urllib.request import urlopen
 import matplotlib.pyplot as plt
 
 def se_text(text):
     '''This embeds a string by turning it into a list then summing the glove values. The out put is the normalized output'''
     l = text.split()     # makes a list for all the words
-    ret= np.zeros(1,50)  #Size of vectors
+    ret= np.zeros((1,50))  #Size of vectors
     for i in l:
         ret+= glove[i]   #Combines all vectors
-    return ret/np.sum(l) #Normalizes
+    return ret/len(l) #Normalizes
     #Do we have repeates
 
 def se_image(path, url):#path is the pickle file
@@ -42,3 +44,5 @@ def find(text, hyp):
         y = lambda item : item[1]
         l.sorted(key=y)
         return l[0]
+#print(se_text("hello world").shape)
+#path="C:\Users\Charles Richards\Desktop\PortableGit\sisearch\resnet18_features.pkl"
